b4bf61304fc62c2ee289a9ec26f08d0111bcfd81
TurikOleksandra <oleksturik@gmail.com>
2025-09-25 01:37:12 +0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,19 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
   compiler: {
-    styledComponents: {
-      displayName: true,
-      ssr: true,
-    },
+    styledComponents: true,
   },
   reactStrictMode: true,
   output: "export",
   trailingSlash: true,
-  basePath: isProd ? "/booking-session.io" : "",
-  assetPrefix: isProd ? "/booking-session.io/" : "",
+  basePath: "/booking-session.io",
+  assetPrefix: "/booking-session.io/",
   images: {
     unoptimized: true,
   },
-  experimental: {
-    forceSwcTransforms: true,
-  },
+  optimizeFonts: false,
+  swcMinify: false,
 };
 
 export default nextConfig;
