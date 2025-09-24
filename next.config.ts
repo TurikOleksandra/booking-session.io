1b1432841c06a55099baa15ca80333c03983dcdc
TurikOleksandra <oleksturik@gmail.com>
2025-09-25 01:50:06 +0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -5,15 +5,6 @@ const nextConfig: NextConfig = {
     styledComponents: true,
   },
   reactStrictMode: true,
-  output: "export",
-  trailingSlash: true,
-  basePath: "/booking-session.io",
-  assetPrefix: "/booking-session.io/",
-  images: {
-    unoptimized: true,
-  },
-  optimizeFonts: false,
-  swcMinify: false,
 };
 
 export default nextConfig;
